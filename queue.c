bf8a8da5ad9fddc52fcd7c9b2034bfaafcae236e
cemanaral <cemanaral425@gmail.com>
2022-01-05 21:00:26 +0300

    whitespaces instead of tabs

--- queue.c
+++ queue.c
@@ -9,38 +9,38 @@ struct queue {
     char** array;
 
     int currentSize;
-	int maxSize;
-	
-	int head;
+    int maxSize;
+    
+    int head;
     int back;
 };
 
 struct queue* createQueue() {
-	struct queue* queue = (struct queue*) malloc(sizeof(struct queue));
-	queue->maxSize = 99999;
-	queue->head = queue->currentSize = 0;
-	queue->back = 99998;
-	queue->array = (char**) malloc(queue->maxSize);
-	return queue;
+    struct queue* queue = (struct queue*) malloc(sizeof(struct queue));
+    queue->maxSize = 99999;
+    queue->head = queue->currentSize = 0;
+    queue->back = 99998;
+    queue->array = (char**) malloc(queue->maxSize);
+    return queue;
 }
 
 void queueAdd(struct queue* queue, char* fileName) {
-	if (queue->currentSize==queue->maxSize)
-		return;
-	queue->back = (queue->back + 1) % queue->maxSize;   
-	queue->array[queue->back] = strdup(fileName);
-	queue->currentSize = queue->currentSize + 1;
+    if (queue->currentSize==queue->maxSize)
+        return;
+    queue->back = (queue->back + 1) % queue->maxSize;   
+    queue->array[queue->back] = strdup(fileName);
+    queue->currentSize = queue->currentSize + 1;
 }
 
 char* queueRemove(struct queue* queue) {
-	if (queue->currentSize) {
+    if (queue->currentSize) {
         char* item = queue->array[queue->head];
-	    queue->head = (queue->head + 1)% queue->maxSize;
-	    queue->currentSize = queue->currentSize - 1;
-	    return item;
+        queue->head = (queue->head + 1)% queue->maxSize;
+        queue->currentSize = queue->currentSize - 1;
+        return item;
     }
     else {
         return NULL;
     }
-	
+    
 }
