176c74634c619facb22a7373d181cc1b73eb67c0
cemanaral <cemanaral425@gmail.com>
2022-01-05 19:39:19 +0300

    Implemented validateArguments extractDirectoryFromArguments extractNumberOfThreadsFromArguments

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+/* Function Headers */
+void validateArguments(int argc);
+char * extractDirectoryFromArguments(char** argv);
+int extractNumberOfThreadsFromArguments(char** argv); 
+
+#define DIRECTORY_INDEX 2
+#define THREAD_NO_INDEX 4
 
 
-int main() {
-    printf("hello world\n");
+
+void validateArguments(int argc) {
+    if (argc != 5) {
+        fprintf(stderr, "ERROR: Invalid arguments\n");
+        fprintf(stderr, "USAGE: ./main.out -d <directoryName> -n <#ofThreads>\n");
+        exit(1);
+    }
+}
+
+char * extractDirectoryFromArguments(char** argv) {
+    return argv[DIRECTORY_INDEX];
+}
+
+int extractNumberOfThreadsFromArguments(char** argv) {
+    return atoi(argv[THREAD_NO_INDEX]);
+}
+
+int main(int argc, char** argv) {
+    validateArguments(argc);    
+    char * directory = extractDirectoryFromArguments(argv);
+    int noOfThreads = extractNumberOfThreadsFromArguments(argv);
+    
     return 0;
 }
